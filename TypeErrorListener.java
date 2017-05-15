8198633fcd6d1d2974ffb655dd1f9f3185632680
adeele <adrianna.malkiewicz@gmail.com>
2017-05-15 23:05:55 +0200

    Overrode important functions

--- TypeErrorListener.java
+++ TypeErrorListener.java
@@ -8,22 +8,45 @@ public class TypeErrorListener extends Python3BaseListener {
         this.issues = new ArrayList<>();
     }
 
-    @Override public void enterFile_input(Python3Parser.File_inputContext ctx) {
-        // File input starts analyze
-        System.out.println("Start");
-    }
-
     @Override public void enterAugassign(Python3Parser.AugassignContext ctx) {
-        // TODO: count certain
         issues.add(new Issue(ctx.start.getLine(), 1, true, ctx.children.get(0).getText()));
     }
 
     @Override public void enterComp_op(Python3Parser.Comp_opContext ctx) {
-        // TODO: count certain
         String operator = ctx.children.get(0).getText();
-        if (operator != "is" && operator != "is not") {
+
+        if (!operator.equals("is") && !operator.equals("is not")) {
             issues.add(new Issue(ctx.start.getLine(), 1, true, operator));
         }
+    }
+
+    @Override public void enterArith_expr(Python3Parser.Arith_exprContext ctx) {
+        if (ctx.children.size() > 1) {
+            issues.add(new Issue(ctx.start.getLine(), 1, true, ctx.children.get(1).getText()));
+        }
+    }
+
+    @Override public void enterTrailer(Python3Parser.TrailerContext ctx) {
+        String bracket = ctx.children.get(0).getText();
+
+        if (bracket.equals("(")) {
+            String function = ctx.parent.getChild(0).getText() + "()";
+            issues.add(new Issue(ctx.start.getLine(), 3, false, function));
+        }
+    }
+
+    @Override public void enterAtom(Python3Parser.AtomContext ctx) {
+        String bracket = ctx.children.get(0).getText();
+
+        if (bracket.equals("[") && ctx.parent.getChildCount() > 1) {
+            String sibling = ctx.parent.getChild(1).getText();
+            issues.add(new Issue(ctx.start.getLine(), 2, false, sibling));
+        }
+    }
+
+    @Override public void enterFor_stmt(Python3Parser.For_stmtContext ctx) {
+        String array = ctx.getChild(3).getText();
+        issues.add(new Issue(ctx.start.getLine(), 1, false, array));
     }
 
     public ArrayList<Issue> getIssues() {
--- out/production/tkom/sample.py
+++ out/production/tkom/sample.py
@@ -1,30 +1,35 @@
-# arithmetic operations
-
-1 + 1
-x + y
-1 + '1'
-
 # assigns
-
 1 += 1
 x += y
 1 += '1'
 
-# functions
+# comparison
+1 < 1
+x < y
+1 < '1'
 
+# comparison - NOT DANGEROUS
+1 is 1
+x is y
+1 is '1'
+
+# arithmetic operations
+1 + 1
+x + y
+1 + '1'
+
+# functions
 len([1, 2])
 len(x)
 len(1)
 
 # non-integer index
-
 [1, 2][1]
 [1, 2][x]
 [1, 2]['1']
 
 # for in loop
-
-for i in [1, 2, 3]:
+for i in [1, 2]:
   print(i)
 
 for i in x:
@@ -34,7 +39,6 @@ for i in 1:
   print(i)
 
 # number of arguments
-
 len([1, 2])
 
 def fun4(**kwargs):
